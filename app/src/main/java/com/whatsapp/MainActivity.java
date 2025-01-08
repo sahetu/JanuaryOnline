462f643ef20c8d6bc27a2dc57b8c5a50ffc92486
Sagar Khatri <khatrisagar2@gmail.com>
2025-01-08 16:05:02 +0530

    Login Form

--- app/src/main/java/com/whatsapp/MainActivity.java
+++ app/src/main/java/com/whatsapp/MainActivity.java
@@ -1,24 +1,48 @@
 package com.whatsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        login = findViewById(R.id.main_login);
+
+        username = findViewById(R.id.main_username);
+        password = findViewById(R.id.main_password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().trim().equals("")){
+                    username.setError("Username Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else{
+                    System.out.println("Login Successfully");
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                    Snackbar.make(view,"Login Successfully",Snackbar.LENGTH_SHORT).show();
+                }
+            }
         });
+
     }
 }
